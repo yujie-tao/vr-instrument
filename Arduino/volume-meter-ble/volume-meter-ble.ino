22677f8d39f6ed881e22a21c9c32e75352e63587
AlexMazursky <alexmazursky@uchicago.edu>
2020-12-06 18:23:31 -0600

    more attempted fixes

---
+++ Arduino/volume-meter-ble/volume-meter-ble.ino
@@ -0,0 +1,218 @@
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+#include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+#include "BluefruitConfig.h"
+
+// mic pin
+int analogPin = A0;
+float val;
+int sampleRaw;
+
+// Low-pass filter just to clean some of the noise
+#include <filters.h>
+const float cutoffFreq   = 10.0;  //Cutoff frequency in Hz
+const float samplingTime = 0.0005; //Sampling time in seconds.
+IIR::ORDER  order  = IIR::ORDER::OD1; // Order (OD1 to OD4)
+Filter filter(cutoffFreq, samplingTime, order);
+float filteredVal;
+
+// thresholds
+const int threshold = 24;
+bool blow = false; // store last state
+
+// timings
+const int debounce = 0; // (ms)
+unsigned long currentTime;
+unsigned long lastTime;
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+      FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+     
+                                Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                                running this at least once is a good idea.
+     
+                                When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                                Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+         
+                                Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+#define FACTORYRESET_ENABLE         0
+#define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+#define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+/*
+  SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+  Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+*/
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(BLUEFRUIT_HWSERIAL_NAME, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  // A baud rate of 115200 is used instead of 9600 for a faster data rate
+  // on non-native USB ports
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command <-> Data Mode Example"));
+  Serial.println(F("------------------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ) {
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+    delay(500);
+  }
+
+  Serial.println(F("******************************"));
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+  }
+
+  // Set module to DATA mode
+  Serial.println( F("Switching to DATA mode!") );
+  ble.setMode(BLUEFRUIT_MODE_DATA);
+
+  Serial.println(F("******************************"));
+}
+
+#define SAMPLES 4 // make it a power of two for best DMA performance
+
+void loop() {
+  // read a bunch of samples:
+  int samples[SAMPLES];
+
+  for (int i=0; i<SAMPLES; i++) {
+    float sample = 0; 
+    while ((sample == 0) || (sample == -1) ) {
+      sample = analogRead(analogPin);
+      //sample = filter.filterIn(sampleRaw);
+    }
+    // convert to 18 bit signed
+    //sample >>= 14; 
+    samples[i] = sample;
+  }
+
+  // ok we hvae the samples, get the mean (avg)
+  float meanval = 0;
+  for (int i=0; i<SAMPLES; i++) {
+    meanval += samples[i];
+  }
+  meanval /= SAMPLES;
+  //Serial.print("# average: " ); Serial.println(meanval);
+
+  // subtract it from all samples to get a 'normalized' output
+  for (int i=0; i<SAMPLES; i++) {
+    samples[i] -= meanval;
+    //Serial.println(samples[i]);
+  }
+
+  // find the 'peak to peak' max
+  float maxsample, minsample;
+  minsample = 100000;
+  maxsample = -100000;
+  for (int i=0; i<SAMPLES; i++) {
+    minsample = min(minsample, samples[i]);
+    maxsample = max(maxsample, samples[i]);
+  }
+  
+  val = maxsample - minsample;
+  val = filter.filterIn(val);
+  Serial.println(val);          // debug value
+  
+  currentTime = millis();
+  
+  // check thresholds to see if a blow 
+  if(int(val) >= threshold && currentTime-lastTime >= debounce){
+    // send 1 for blow duration
+    ble.print(1);
+    blow = true;
+    lastTime = millis();
+  }
+//  else if(blow == true && currentTime-lastTime >= debounce){
+//    ble.print(0);
+//    blow = false;
+//  }
+  else{
+    ble.print(0);
+  }
+  //ble.println();
+}
